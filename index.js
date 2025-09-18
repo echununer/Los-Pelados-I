46d0019e6add26cb44777a0b52f1c3b94233214a
echununer <echununerpro@gmail.com>
2025-09-18 12:08:27 -0300

    Arranque el base

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ import {
 
 // Cargar sabores y productos
 // COMPLETEN USTEDES
+import fs from"fs"
 let sabores;
+sabores= JSON.parse(fs.readFileSync("data/sabores.json", utf-8))
+
 let productos;
+productos= JSON.parse(fs.readFileSync("data/productos.json", utf-8))
 
 // Ingresar cliente
 let cliente = ingresarCliente();
